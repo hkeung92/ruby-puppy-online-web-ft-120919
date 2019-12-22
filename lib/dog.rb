07ebf3b1faff02aca0209d008b8d03019eb04d86
Ho Yin Keung <hykeung92@gmail.com>
2019-12-22 21:30:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,12 +9,10 @@ class Dog
   end 
   
   def self.all 
-    @@all.each do |dog|
-		puts dog.name
+    @@all
 	end
   
-#   def self.print_all
-#     self.each {|name| puts name}
-#     name
-#   end
+  def self.print_all
+    puts @@all.map{ |dog| dog.name }
+  end
 end
